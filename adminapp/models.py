b8b963b069e2e611b71cf5b29a9f771fc043480a
Praveenh682 <138794818+Praveenh682@users.noreply.github.com>
2024-10-21 12:38:59 +0530

    [MOD]iumaster

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+# to store host names to differentiate the user based on the host 
+class IUMaster(models.Model):
+    name=models.CharField(max_length=50)
+    domain=models.CharField(max_length=50) #host name 
+    contact_mobile_no=models.CharField(max_length=15)
+    logo=JSONField(default=dict) # logo image stored as json 
+    address=models.CharField(max_length=250,null=True,blank=True)
+    city=models.CharField(max_length=50,null=True,blank=True)
+    state=models.CharField(max_length=50,null=True,blank=True)
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    created_by=models.IntegerField(blank=True,null=True)
+    modified_at=models.DateTimeField(auto_now_add=True)
+    modified_by=models.IntegerField(blank=True,null=True)
+
+    class Meta:
+        db_table = 'iumaster'
+        ordering = ['-created_at']
+
+
