86ac73f4774e0516a78da0ca8fd31c5c54175685
vicky <vigneshganapathi1006@gmail.com>
2024-10-22 18:00:25 +0530

    [ADD]add the order and payment model

--- order/models.py
+++ order/models.py
@@ -78,6 +78,78 @@ class ProductVariation(BaseModel):
         db_table='product_variation'
         ordering = ['created_at']
 
+"""fetch the whole order for specific user"""
+class OrderDetails(models.Model):
+    #store the user where user is in CustomUser
+    user = models.ForeignKey(CustomUser, related_name='OrderDetails', on_delete=models.CASCADE)
+    #fetch whole product and its price stored in it
+    total_price=models.DecimalField(null=True,blank=True,default=0,decimal_places=3)
+    #store the iu id from the IUMaster
+    iu_id=models.ForeignKey(IUMaster,related_name='OrderDetails',on_delete=models.CASCADE)
+   
+   
+    class Meta:
+        db_table = 'order_details'
+        ordering = ['created_at']
+
+"""store the order items one by one from order details for specific user"""    
+class OrderItems(BaseModel):
+    #store the user where user is in CustomUser
+    user = models.ForeignKey(CustomUser, related_name='OrderItems', on_delete=models.CASCADE)
+    #fetch specific user order from the OrderDetails and store it 
+    order=models.ForeignKey(OrderDetails, related_name='OrderItems',on_delete=models.CASCADE)
+    #store the product where in order
+    product=models.ForeignKey(ProductMaster,related_name='OrderItems',on_delete=models.CASCADE)
+    #store the variant where in order
+    variation=models.ForeignKey(VariantOption,related_name='OrderItems',on_delete=models.CASCADE)
+    #fetch the seller from the product details
+    seller=models.ForeignKey(SellerProfile,related_name='OrderItems',on_delete=models.CASCADE)
+    #fetch the quantity from specific product 
+    quantity=models.IntegerField(null=True,blank=True)
+    #fetch the price from specific product 
+    price=models.DecimalField(null=True,blank=True,default=0,decimal_places=3)
+    #deliver status
+    order_status=models.CharField(max_length=50,default="pending")
+    #product delivered location
+    delivered_location= models.TextField(null=True, blank=True)
+    #product delerived time
+    delivered_time=models.DateTimeField(null=True, blank=True)
+    #if buyer return product then its changed to True
+    is_returned=models.BooleanField(default=False)
+    #then buyer enter its location for pickup the product
+    return_pickup_location=models.CharField( max_length=250,null=True, blank=True)
+    #buyer explain about the return product
+    return_reason=models.TextField(blank=True,null=True)
+    #product return process approved by whom is stored in it
+    approved_by=models.IntegerField(blank=True, null=True)
+    return_completed_time=models.DateTimeField(null=True, blank=True)
+    #store the iu id from the IUMaster
+    iu_id=models.ForeignKey(IUMaster,related_name='OrderItems',on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'order_items'
+        ordering = ['created_at']
+        
+class PaymentTypeMaster(BaseModel):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=50,blank=True,null=True)
+    #store the iu id from the IUMaster
+    iu_id=models.ForeignKey(IUMaster,related_name='PaymentTypeMaster',on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'payment_type'
+        ordering = ['created_at']
+        
+class PaymentDetails(BaseModel):
+    #fetch specific user order from the OrderDetails and store it 
+    order=models.ForeignKey(OrderDetails, related_name='PaymentDetails',on_delete=models.CASCADE)
+    payment_type=models.ForeignKey(PaymentTypeMaster, related_name='PaymentDetails',on_delete=models.CASCADE)
+    paid_amount=models.DecimalField(null=True,blank=True,default=0,decimal_places=3)
+    payment_status=models.CharField(max_length=50,default="pending")
+    return_amount=models.DecimalField(null=True,blank=True,default=0,decimal_places=3)
+    
+    
+
 
 
  
