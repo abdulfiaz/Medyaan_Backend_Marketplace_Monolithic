990709cce8028889ffd2b88e2110219f56090f31
Praveenh682 <138794818+Praveenh682@users.noreply.github.com>
2024-10-29 11:31:22 +0530

    [MOD]IUMaster

--- adminapp/views.py
+++ adminapp/views.py
@@ -3,16 +3,15 @@ from adminapp.models import *
 from rest_framework.views import APIView,status
 from rest_framework.response import Response
 from django.db import transaction
-from adminapp.rolename import *
+from users.auth import get_user_roles
 from adminapp.serializers import IUMasterSerializer
 
 
 
 class IUMasterAPI(APIView):
-    serializer_class=IUMasterSerializer()
     def get(self, request):
         iu_id = request.query_params.get('id')
-        rolename = getrolename(request)
+        rolename = get_user_roles(request)
 
         if rolename != 'ADMIN':
             return Response({"status": "error", "message": "Only ADMIN can have the access!"}, status=status.HTTP_403_FORBIDDEN)
@@ -22,7 +21,7 @@ class IUMasterAPI(APIView):
                 iumaster = IUMaster.objects.filter(id=iu_id, is_active=True)
                 if not iumaster:
                     return Response({"status": "error", "message": "IUMaster not found"}, status=status.HTTP_404_NOT_FOUND)
-                serializer = self.serializer_class(iumaster)
+                serializer = IUMasterSerializer(iumaster)
             else:
                 iumaster = IUMaster.objects.filter(is_active=True)
                 serializer = IUMasterSerializer(iumaster, many=True)
@@ -33,46 +32,50 @@ class IUMasterAPI(APIView):
             return Response({"status": "error", "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request):
-        rolename = getrolename(request)
+        rolename = get_user_roles(request)
         if rolename != "ADMIN":
             return Response({"status": "error", "message": "Only ADMIN can access this!"}, status=status.HTTP_403_FORBIDDEN)
 
+        transaction.set_autocommit(False)
+        data=request.data
+        data['created_by']=request.user.id
+        serializer = IUMasterSerializer(data=data)
         try:
-            with transaction.atomic():
-                serializer = IUMasterSerializer(data=request.data)
-                if serializer.is_valid():
-                    iumaster = serializer.save(created_by=request.user)
-                    return Response({"status": "success", "message": "Details created successfully"}, status=status.HTTP_201_CREATED)
-                return Response({"status": "error", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-        except Exception as e:
-            return Response({"status": "error", "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            if serializer.is_valid():
+                serializer.save()
+                transaction.commit()
+                return Response({"status": "success", "message": "Details created successfully"}, status=status.HTTP_201_CREATED)
+        except :
+            transaction.rollback()
+            return Response({"status": "error", "message":  serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request):
         iu_id = request.data.get('id')
-        rolename = getrolename(request)
+        rolename = get_user_roles(request)
 
         if rolename != "ADMIN":
             return Response({"status": "error", "message": "Only ADMIN can access this!"}, status=status.HTTP_403_FORBIDDEN)
 
+        
+        iumaster = IUMaster.objects.filter(id=iu_id, is_active=True)
+        if not iumaster:
+            return Response({"status": "error", "message": "IUMaster not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        transaction.set_autocommit(False)
+        serializer = IUMasterSerializer(iumaster, data=request.data, partial=True)
         try:
-            iumaster = IUMaster.objects.filter(id=iu_id, is_active=True)
-            if not iumaster:
-                return Response({"status": "error", "message": "IUMaster not found"}, status=status.HTTP_404_NOT_FOUND)
-
-            with transaction.atomic():
-                serializer = IUMasterSerializer(iumaster, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save(modified_by=request.user)
-                    return Response({"status": "success", "message": "Details updated successfully"}, status=status.HTTP_200_OK)
-                return Response({"status": "error", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
+            if serializer.is_valid():
+                serializer.save(modified_by=request.user)
+                return Response({"status": "success", "message": "Details updated successfully"}, status=status.HTTP_200_OK)
+            transaction.commit()
+        
         except Exception as e:
-            return Response({"status": "error", "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            transaction.rollback()
+            return Response({"status": "error", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
         iu_id = request.data.get('id')
-        rolename = getrolename(request)
+        rolename = get_user_roles(request)
 
         if rolename != "ADMIN":
             return Response({"status": "error", "message": "Only ADMIN can access this!"}, status=status.HTTP_403_FORBIDDEN)
