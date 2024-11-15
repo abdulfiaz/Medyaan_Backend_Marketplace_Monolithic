1194ea5693f32ab157173f882409d4f5f940dc49
vish140503 <vishalakshik14503@gmail.com>
2024-11-15 17:59:40 +0530

    [MOD]

--- adminapp/utils.py
+++ adminapp/utils.py
@@ -1,7 +1,7 @@
 
 from notification.utils import create_notification
 
-def overallnotification(sender_id, receiver_id, event, subject, message, notification_message, iu_id, request_user):
+def overallnotification(sender_id, receiver_id, event, subject, message, notification_message, iu_id, request_user,email_content=None):
     data = {
         'sender': sender_id,
         'receiver': receiver_id,
@@ -10,7 +10,8 @@ def overallnotification(sender_id, receiver_id, event, subject, message, notific
         'message': message,
         'notification_message': notification_message,
         'redirect_link': "https://example.com/",
-        'iu_id': iu_id
+        'iu_id': iu_id,
+        'email_content': email_content
     }
     notification_response = create_notification(data, request_user)
     return notification_response
